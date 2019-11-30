655e3cd2dd1e8149efcfd31691e2f150781fb907
Vladislav <vladislavluksha99@gmail.com>
2019-11-30 18:04:33 +0300

    Initial version of the project

---
+++ dev-5/dev-5/PageObjects/PageObject.cs
@@ -0,0 +1,80 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System; 
+
+namespace dev_5
+{
+    /// <summary>
+    /// This is abstract page object class
+    /// </summary>
+    abstract public class PageObject
+    {
+        protected By exitLocator = By.CssSelector("a[title ='выход']");
+        protected By emailLocator = By.CssSelector("a[class='x-ph__link x-ph__link_first']");
+        protected By submitLocator = By.CssSelector("span[title ='Отправить']");
+        protected By writeMessageLocator = By.XPath("/html/body/div[15]/div[2]/div/div[1]/div[2]/div[3]/div[5]/div/div/div[2]/div[1]/div[1]/div[1]");
+
+        protected IWebDriver WebDriver { get; set; }
+        protected WebDriverWait Wait { get; set; }
+        protected IWebElement NavigationButton { get; set; }
+        protected IWebElement ElementButton { get; set; }
+        /// <summary>
+        /// This is class constructor
+        /// </summary>
+        /// <param name="webDriver"></param>
+        public PageObject(IWebDriver webDriver)
+        {
+            this.WebDriver = webDriver;
+            Wait = new WebDriverWait(WebDriver, TimeSpan.FromSeconds(20));
+        }
+        /// <summary>
+        /// This method leads on button
+        /// </summary>
+        /// <param name="locator"></param>
+        public void NavigateOnButton(By locator)
+        {
+            try
+            {
+                Wait.Until(t => WebDriver.FindElement(locator).Displayed);
+                NavigationButton = WebDriver.FindElement(locator);
+                NavigationButton.Click();
+            }
+            catch(UnhandledAlertException)
+            {
+
+            }
+        }
+        /// <summary>
+        /// This method leaves the email
+        /// </summary>
+        public void OutOfEmail()
+        {
+            NavigateOnButton(exitLocator);
+            NavigateOnButton(emailLocator);
+        }
+        /// <summary>
+        /// This method searches for elements
+        /// </summary>
+        /// <param name="locator"></param>
+        /// <returns>ElementButton</returns>
+        public IWebElement FindElement(By locator)
+        {
+            Wait.Until(t => WebDriver.FindElement(locator).Displayed);
+            ElementButton = WebDriver.FindElement(locator);
+            return ElementButton;
+        }
+        /// <summary>
+        /// This method enters data
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="locator"></param>
+        /// <returns>ElementButton</returns>
+        public  IWebElement DataInput(string text, By locator)
+        {
+            ElementButton = FindElement(locator);
+            ElementButton.Click();
+            ElementButton.SendKeys(text + Keys.Enter);
+            return ElementButton;
+        }
+    }
+}
---
+++ dev-5/dev-5/PageObjects/SenderInboxPage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using OpenQA.Selenium;
+
+namespace dev_5
+{
+    /// <summary>
+    /// This is sender inbox page class
+    /// </summary>
+    public class SenderInboxPage:PageObject
+    {
+        By userNameLocator = By.CssSelector("span[title='Vladislav Luksha <luksha.vladislav@mail.ru>']");
+        By writeLetterLocator = By.CssSelector("span.compose-button__wrapper");
+        By senderSelectLocator = By.CssSelector("label.container--zU301");
+        By themeLocator = By.CssSelector("input[name='Subject']");
+   
+        IWebElement Username { get; set; }
+
+        /// <summary>
+        /// This is constructor class
+        /// </summary>
+        /// <param name="webDriver"></param>
+        public SenderInboxPage(IWebDriver webDriver) : base(webDriver) { }
+
+        /// <summary>
+        /// This method writes messages
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="theme"></param>
+        /// <param name="message"></param>
+        public void WriteLetter(string sender,string theme,string message)
+        {
+            NavigateOnButton(writeLetterLocator);
+            DataInput(sender, senderSelectLocator);
+            DataInput(theme, themeLocator);
+            DataInput(message,writeMessageLocator);
+            NavigateOnButton(submitLocator);   
+        }
+        /// <summary>
+        /// This method checks received messages
+        /// </summary>
+        /// <returns> true or false </returns>
+        public bool CheckReceivedMessage()
+        {
+            Username = FindElement(userNameLocator);
+            return (Username!=null);
+        }
+    }
+}
