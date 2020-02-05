c5d4bc91181d6e50cf8d87770395a8e652e924b0
Vladislav <vladislavluksha99@gmail.com>
2020-02-05 14:13:43 +0300

    Add comment and error correction

--- SearchLettersInAString/Line.cs
+++ SearchLettersInAString/Line.cs
@@ -2,23 +2,33 @@
 
 namespace SearchLettersInAString
 {
+    /// <summary>
+    /// This is string class
+    /// </summary>
     class Line
     {
-        string line;
+        string String { get; set; }
         const int startCounter = 1;
 
+        /// <summary>
+        /// This is constructor class
+        /// </summary>
+        /// <param name="line"></param>
         public Line(string line)
         {
-            this.line = line;
+            this.String = line;
         }
-
+        /// <summary>
+        /// This method searches for the maximum number of identical characters 
+        /// </summary>
+        /// <returns>maxNumberOfSymbols</returns>
         public int SearchForTheMaxNumberOfSymbols()
         {
             int currentNumberOfSymbols = 0;
             int maxNumberOfSymbols = 0;
-            for (int i = 1; i < line.Length; i++)
+            for (int i = 1; i < String.Length; i++)
             {
-                if (line[i - 1] == line[i])
+                if (String[i - 1] == String[i])
                 {
                     currentNumberOfSymbols++;
                 }
